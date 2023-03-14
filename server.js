4b71d30ce572180400d5a7a4889aa05f9493b2ee
remku <ngancloverr+remkugithub@gmail.com>
2023-03-14 13:52:54 +0530

    reels allowed

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const os = require("os");
-const puppeteer = require("puppeteer");
+const axios = require("axios").default;
+const cheerio = require("cheerio");
+// const puppeteer = require("puppeteer");
 // if not in production import dotenv
 const isDevelopment = process.env.NODE_ENV !== "development";
 if (isDevelopment) {
@@ -18,14 +20,27 @@ app.use(express.json());
 app.use(cors());
 
 // Puppeteer
-const puppeteerFn = async (url) => {
-  const browser = await puppeteer.launch({ headless: true });
-  const newPage = await browser.newPage();
+// const puppeteerFn = async (url) => {
+//   const browser = await puppeteer.launch({ headless: true });
+//   const newPage = await browser.newPage();
 
-  await newPage.goto(url);
-  const aTag = await newPage.waitForSelector(".widePic > a");
-  const fullLink = await aTag.evaluate((e) => e.href);
+//   await newPage.goto(url);
+//   const aTag = await newPage.waitForSelector(".widePic > a");
+//   const fullLink = await aTag.evaluate((e) => e.href);
 
+//   const link = decodeURIComponent(
+//     fullLink.replace(new RegExp(".+src=", "gi"), "")
+//   );
+
+//   return link;
+// };
+
+const fetchLink = async (url) => {
+  const response = await axios.get(url);
+  const html = response.data;
+
+  const $ = cheerio.load(html);
+  const fullLink = $("a", ".widePic").attr("href");
   const link = decodeURIComponent(
     fullLink.replace(new RegExp(".+src=", "gi"), "")
   );
@@ -35,16 +50,28 @@ const puppeteerFn = async (url) => {
 
 // API Request
 app.post("/getFbVideoLink", async (req, res) => {
-  const { url } = req.body;
+  try {
+    const { url } = req.body;
 
-  const search = new RegExp("www", "gi");
+    const response = await axios.get(url);
+    const directUrl = response.request.res.responseUrl;
 
-  const mbasicUrl = url.replace(search, "mbasic");
+    let search = new RegExp("www", "gi");
+    let mbasicUrl = directUrl.replace(search, "mbasic");
 
-  const link = await puppeteerFn(mbasicUrl);
-  res.json({
-    downloadLink: link,
-  });
+    if (directUrl.includes("facebook.com/reel/")) {
+      mbasicUrl =
+        `https://mbasic.facebook.com/watch?v=` +
+        directUrl.match(new RegExp("[0-9]+", "g"))[0];
+    }
+
+    const link = await fetchLink(mbasicUrl);
+    res.json({
+      downloadLink: link,
+    });
+  } catch (err) {
+    res.status(404).json({ message: "Video not found" });
+  }
 });
 
 app.listen(PORT, () => console.log(`http://${hostName}:${PORT}`));
